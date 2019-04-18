29d4a2b04e3335fa9c5fba10111ea722338bdf3e
Sarath Racherla <sharathsaee@gmail.com>
2019-04-18 23:33:57 +0530

    updated packer file
    updated packer file

---
+++ ec2InstanceLaunch.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "MyInstancefromPacker"{
+  instance_type = "t2.micro",
+  image_id = "${var.ami}",
+  key_name="KeyPair-Ohio"
+}
--- example.sh
+++ example.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+sudo su
+cd
+apt-get update -y
+apt-get install apache2 -y
+service apache2 start
+chkconfig apache2 on
+cd /var/www/html
 echo "hello"
